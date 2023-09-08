2122fa112343c4fb2fb960d5e207860ea9a04817
Sakshi Sahu <117566446+sakshisahu612@users.noreply.github.com>
2023-09-08 15:14:19 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (71.43%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     vector<int> getRow(int row) {
+        if(row == 0){
+            return {1};
+        }
         long long ans = 1;
         vector<int> ansRow(row+1,1);
-        //ansRow.push_back(1);
         for(int col = 1; col<row; col++){
             ans = ans*(row-col+1);
             ans = ans/(col);
